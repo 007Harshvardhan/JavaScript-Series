e32fe7429c820e41875af7ee02dd8bcacec91249
Harsh Vardhan <115665919+007Harshvardhan@users.noreply.github.com>
2025-09-30 19:03:23 +0000

    conversion Of DataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score="33"
+
+console.log(typeof score)
+//2nd method to check type
+console.log(typeof(score))
+
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+
+
+//"33" => 33
+//"33aaa" => NaN
+//true => 1
+//false =>0
+
+
+let isLoggedin="hars"
+
+let booleanisLoggedin = Boolean(isLoggedin)
+console.log(booleanisLoggedin)
+
+
+// 1c => true
+//0 => false
+//"" =>false
+//"harsh" => true
+

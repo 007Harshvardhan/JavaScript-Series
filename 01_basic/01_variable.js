e8a8f2733d9e352a6d79192c3eedf8488b38726c
Harsh Vardhan <115665919+007Harshvardhan@users.noreply.github.com>
2025-09-30 18:18:07 +0000

    Variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=13221221
+let accountEmail="haehsshd@gmail.com"
+var accountPassword="121212"
+accountCity="new work"
+let accountStete;
+//accountId =2 //not alowed
+accountEmail="hac@gmail.com"
+accountPassword="12211"
+accountCity="lasvegas"
+
+
+console.log(accountId);
+/*
+Prefer not to use var
+beacause of issue in block scope and fuctional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStete])

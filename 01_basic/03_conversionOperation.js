4ded75ea32a28369e97c1a8a4e4022dea365a9c7
Harsh Vardhan <115665919+007Harshvardhan@users.noreply.github.com>
2025-10-01 06:20:14 +0000

    comparision

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,32 +1,55 @@
-let score="33"
+// let score="33"
 
-console.log(typeof score)
-//2nd method to check type
-console.log(typeof(score))
-
-
-let valueInNumber=Number(score)
-
-console.log(typeof valueInNumber)
-
-console.log(valueInNumber)
+// console.log(typeof score)
+// //2nd method to check type
+// console.log(typeof(score))
 
 
+// let valueInNumber=Number(score)
 
-//"33" => 33
-//"33aaa" => NaN
-//true => 1
-//false =>0
+// console.log(typeof valueInNumber)
 
-
-let isLoggedin="hars"
-
-let booleanisLoggedin = Boolean(isLoggedin)
-console.log(booleanisLoggedin)
+// console.log(valueInNumber)
 
 
-// 1c => true
-//0 => false
-//"" =>false
-//"harsh" => true
+
+// //"33" => 33
+// //"33aaa" => NaN
+// //true => 1
+// //false =>0
+
+
+// let isLoggedin="hars"
+
+// let booleanisLoggedin = Boolean(isLoggedin)
+// console.log(booleanisLoggedin)
+
+
+// // 1c => true
+// //0 => false
+// //"" =>false
+// //"harsh" => true
+
+
+
+
+// Operations 
+let value=10
+let negValue =-value
+
+
+
+let str1="hello"
+let str2="HARSH"
+
+let str3=str1+str2
+
+console.log(str3)
+
+console.log("1" +2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1" +2 +2)
+console.log(1+2+"2")
+
 
